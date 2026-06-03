b8b419991b8f6fc47c4ca737674d9d8e17374482
SHAH MRUDUL JIGISHKUMAR <147684023+MrudulShah24@users.noreply.github.com>
2026-06-03 14:57:38 +0530

    Delete React directory

--- React/eduforge-server/routes/recommendationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { getRecommendations } = require('../controllers/recommendationController');
-
-// Get personalized recommendations for the logged-in user
-router.get('/', protect, getRecommendations);
-
-module.exports = router;
